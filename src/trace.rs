f9379adc85f8e86b5fd733e87bf6c5e289eac5c4
dssgabriel <dss.gabriel@protonmail.com>
2021-12-05 23:57:44 +0100

    Adapted tests to the new and improved `Event` enum

--- src/trace.rs
+++ src/trace.rs
@@ -1,4 +1,4 @@
-use crate::event::Event;
+use crate::event::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -18,10 +18,10 @@ impl Trace {
 
 #[cfg(test)]
 mod tests {
-    use crate::event::{Event, MpiOp, WORLD};
+    use crate::WORLD;
+    use super::*;
     use std::fs::File;
     use std::io::Write;
-    use super::*;
 
     #[test]
     fn builds() {
@@ -32,17 +32,7 @@ mod tests {
     #[test]
     fn adds_event() {
         let mut t = Trace::new();
-        let e = Event::new(
-            MpiOp::Irecv {
-                current_rank: 1,
-                partner_rank: 0,
-                comm: WORLD,
-                tag: 0,
-                req: 0,
-            },
-            69,
-            420,
-        );
+        let e = Event::Irecv(NonBlocking::new(1, 0, WORLD, 0, 0, 69, 420));
 
         t.add_event(e.clone());
         assert_eq!(t.events[0], e);
@@ -51,55 +41,12 @@ mod tests {
     #[test]
     fn serialize_to_file() {
         let mut t = Trace::new();
-        let e0 = Event::new(MpiOp::Init, 3, 7);
-        let e1 = Event::new(
-            MpiOp::Isend {
-                current_rank: 0,
-                partner_rank: 1,
-                comm: WORLD,
-                tag: 0,
-                req: 0,
-            },
-            9,
-            19,
-        );
-        let e2 = Event::new(
-            MpiOp::Wait {
-                current_rank: 0,
-                comm: WORLD,
-                req: 0,
-            },
-            20,
-            27,
-        );
-        let e3 = Event::new(
-            MpiOp::Irecv {
-                current_rank: 0,
-                partner_rank: 1,
-                comm: WORLD,
-                tag: 1,
-                req: 1,
-            },
-            69,
-            420,
-        );
-        let e4 = Event::new(
-            MpiOp::Wait {
-                current_rank: 0,
-                comm: WORLD,
-                req: 1,
-            },
-            555,
-            567,
-        );
-        let e5 = Event::new(
-            MpiOp::Finalize {
-                current_rank: 0,
-                comm: WORLD,
-            },
-            978,
-            1024,
-        );
+        let e0 = Event::Init(Init::new(34, 0.78));
+        let e1 = Event::Isend(NonBlocking::new(0, 1, WORLD, 0, 0, 9, 19));
+        let e2 = Event::Wait(Wait::new(0, WORLD, 0, 20, 27));
+        let e3 = Event::Irecv(NonBlocking::new(0, 1, WORLD, 1, 1, 69, 420));
+        let e4 = Event::Wait(Wait::new(0, WORLD, 1, 555, 567));
+        let e5 = Event::Finalize(Finalize::new(0, 978, 1024f64));
 
         t.add_event(e0);
         t.add_event(e1);
@@ -109,9 +56,9 @@ mod tests {
         t.add_event(e5);
 
         let serialized = serde_json::to_string_pretty(&t).expect("Failed to serialize");
-        let mut file = File::create("/tmp/test.json").unwrap();
+        let mut file = File::create("./target/test.json").unwrap();
         write!(file, "{}", serialized).unwrap();
 
-        assert_eq!(std::path::Path::new("/tmp/test.json").exists(), true);
+        assert_eq!(std::path::Path::new("./target/test.json").exists(), true);
     }
 }
