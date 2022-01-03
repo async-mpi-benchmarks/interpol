d2f66f5ea5a303b1765e83e09d79712901156050
dssgabriel <dss.gabriel@protonmail.com>
2022-01-04 00:48:53 +0100

    Added documentation + separated `Event` inits & locking the `Mutex`

--- interpol-rs/src/interpol.rs
+++ interpol-rs/src/interpol.rs
@@ -13,14 +13,14 @@ lazy_static! {
     static ref TRACES: Mutex<Vec<Event>> = Mutex::new(vec![]);
 }
 
+/// Registers an `MPI_Init` call into the static `TRACES` vector.
 #[no_mangle]
 pub extern "C" fn register_init(cycles: u64, time: f64) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Init(Init::new(cycles, time)));
+    let init_event = Event::Init(Init::new(cycles, time));
+    TRACES.lock().unwrap().push(init_event);
 }
 
+/// Registers an `MPI_Send` call into the static `TRACES` vector.
 #[no_mangle]
 pub extern "C" fn register_send(
     cycles_lo: u64,
@@ -31,20 +31,19 @@ pub extern "C" fn register_send(
     partner_rank: i32,
     tag: i32,
 ) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Send(Blocking::new(
-            cycles_lo,
-            cycles_hi,
-            bytes,
-            comm,
-            current_rank,
-            partner_rank,
-            tag,
-        )));
+    let send_event = Event::Send(Blocking::new(
+        cycles_lo,
+        cycles_hi,
+        bytes,
+        comm,
+        current_rank,
+        partner_rank,
+        tag,
+    ));
+    TRACES.lock().unwrap().push(send_event);
 }
 
+/// Registers an `MPI_Recv` call into the static `TRACES` vector.
 #[no_mangle]
 pub extern "C" fn register_recv(
     cycles_lo: u64,
@@ -55,20 +54,19 @@ pub extern "C" fn register_recv(
     partner_rank: i32,
     tag: i32,
 ) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Recv(Blocking::new(
-            cycles_lo,
-            cycles_hi,
-            bytes,
-            comm,
-            current_rank,
-            partner_rank,
-            tag,
-        )));
+    let recv_event = Event::Recv(Blocking::new(
+        cycles_lo,
+        cycles_hi,
+        bytes,
+        comm,
+        current_rank,
+        partner_rank,
+        tag,
+    ));
+    TRACES.lock().unwrap().push(recv_event);
 }
 
+/// Registers an `MPI_Isend` call into the static `TRACES` vector.
 #[no_mangle]
 pub extern "C" fn register_isend(
     cycles_lo: u64,
@@ -80,21 +78,20 @@ pub extern "C" fn register_isend(
     partner_rank: i32,
     tag: i32,
 ) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Isend(NonBlocking::new(
-            cycles_lo,
-            cycles_hi,
-            bytes,
-            comm,
-            req,
-            current_rank,
-            partner_rank,
-            tag,
-        )));
+    let isend_event = Event::Isend(NonBlocking::new(
+        cycles_lo,
+        cycles_hi,
+        bytes,
+        comm,
+        req,
+        current_rank,
+        partner_rank,
+        tag,
+    ));
+    TRACES.lock().unwrap().push(isend_event);
 }
 
+/// Registers an `MPI_Irecv` call into the static `TRACES` vector.
 #[no_mangle]
 pub extern "C" fn register_irecv(
     cycles_lo: u64,
@@ -106,50 +103,39 @@ pub extern "C" fn register_irecv(
     partner_rank: i32,
     tag: i32,
 ) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Irecv(NonBlocking::new(
-            cycles_lo,
-            cycles_hi,
-            bytes,
-            comm,
-            req,
-            current_rank,
-            partner_rank,
-            tag,
-        )));
+    let irecv_event = Event::Irecv(NonBlocking::new(
+        cycles_lo,
+        cycles_hi,
+        bytes,
+        comm,
+        req,
+        current_rank,
+        partner_rank,
+        tag,
+    ));
+    TRACES.lock().unwrap().push(irecv_event);
 }
 
+/// Registers an `MPI_Wait` call into the static `TRACES` vector.
 #[no_mangle]
-pub extern "C" fn register_wait(
-    cycles_lo: u64,
-    cycles_hi: u64,
-    req: MpiReq,
-    current_rank: i32,
-) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Wait(Wait::new(
-            cycles_lo,
-            cycles_hi,
-            req,
-            current_rank,
-        )));
+pub extern "C" fn register_wait(cycles_lo: u64, cycles_hi: u64, req: MpiReq, current_rank: i32) {
+    let wait_event = Event::Wait(Wait::new(cycles_lo, cycles_hi, req, current_rank));
+    TRACES.lock().unwrap().push(wait_event);
 }
 
+/// Registers an `MPI_Finalize` call into the static `TRACES` vector.
+///
+/// As this should be the final registered event, serialize the contents of the `TRACES` vector.
 #[no_mangle]
 pub extern "C" fn register_finalize(cycles: u64, time: f64, rank: i32) {
-    TRACES
-        .lock()
-        .expect("Failed to take the lock")
-        .push(Event::Finalize(Finalize::new(cycles, time, rank)));
+    let finalize_event = Event::Finalize(Finalize::new(cycles, time, rank));
+    TRACES.lock().unwrap().push(finalize_event);
 
+    // Take the lock on the `Mutex<Vec<T>>` before serializing
     let guard = TRACES.lock().unwrap();
 
-    let serialized = serde_json::to_string_pretty(&*guard).expect("Failed to serialize");
-    let filename = format!("rank{}_traces.json", rank.to_string());
-    let mut file = File::create(filename).expect("Failed to create serialization file");
-    write!(file, "{}", serialized).expect("Failed to write to serialization file");
+    // Serialize the contents of the `Vec` and write them to an output file
+    let ser_traces = serde_json::to_string_pretty(&*guard).unwrap();
+    let mut file = File::create(format!("rank{}_traces.json", rank.to_string())).unwrap();
+    write!(file, "{}", ser_traces).unwrap();
 }
