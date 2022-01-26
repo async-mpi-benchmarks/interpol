f72f505e6e6300299da3a4681990e6ba1fb2a5e0
dssgabriel <dss.gabriel@protonmail.com>
2022-01-26 16:04:44 +0100

    Fixed datatype size calculation

--- src/interpol.c
+++ src/interpol.c
@@ -14,7 +14,7 @@ uint32_t jenkins_one_at_a_time_hash(char *key, size_t len)
 {
     uint32_t hash = 0;
 
-    for(size_t i = 0; i < len; i++) {
+    for (size_t i = 0; i < len; i++) {
         hash += key[i];
         hash += (hash << 10);
         hash ^= (hash >> 6);
@@ -52,14 +52,15 @@ int MPI_Send(const void *buf, int count, MPI_Datatype datatype, int dest,
     int ret = PMPI_Send(buf, count, datatype, dest, tag, comm);
     uint64_t cycles_hi = rdtsc();
 
-    size_t bytes = sizeof(datatype) * count;
+    ssize_t bytes;
+    PMPI_Type_size(datatype, &bytes);
+    bytes *= count;
     int comm_f = PMPI_Comm_c2f(comm);
 
-    register_send(cycles_lo, cycles_hi, bytes,
+    register_send(cycles_lo, cycles_hi, (size_t)bytes,
                   comm_f, proc_rank, dest, tag);
     return ret;
 }
-
 
 int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
              MPI_Comm comm, MPI_Status *status)
@@ -68,10 +69,12 @@ int MPI_Recv(void *buf, int count, MPI_Datatype datatype, int source, int tag,
     int ret = PMPI_Recv(buf, count, datatype, source, tag, comm, status);
     uint64_t cycles_hi = rdtsc();
 
-    size_t bytes = sizeof(datatype) * count;
+    ssize_t bytes;
+    MPI_Type_size(datatype, &bytes);
+    bytes *= count;
     int comm_f = PMPI_Comm_c2f(comm);
 
-    register_recv(cycles_lo, cycles_hi, bytes,
+    register_recv(cycles_lo, cycles_hi, (size_t)bytes,
                   comm_f, proc_rank, source, tag);
     return ret;
 }
@@ -83,11 +86,13 @@ int MPI_Isend(const void *buf, int count, MPI_Datatype datatype, int dest,
     int ret = PMPI_Isend(buf, count, datatype, dest, tag, comm, request);
     uint64_t cycles_hi = rdtsc();
 
-    size_t bytes = sizeof(datatype) * count;
+    ssize_t bytes;
+    PMPI_Type_size(datatype, &bytes);
+    bytes *= count;
     int comm_f = PMPI_Comm_c2f(comm);
     uint32_t req_f = jenkins_one_at_a_time_hash((char *)request, sizeof(MPI_Request));
 
-    register_isend(cycles_lo, cycles_hi, bytes,
+    register_isend(cycles_lo, cycles_hi, (size_t)bytes,
                    comm_f, req_f, proc_rank, dest, tag);
     return ret;
 }
@@ -99,11 +104,13 @@ int MPI_Irecv(void *buf, int count, MPI_Datatype datatype, int source,
     int ret = PMPI_Irecv(buf, count, datatype, source, tag, comm, request);
     uint64_t cycles_hi = rdtsc();
 
-    size_t bytes = sizeof(datatype) * count;
+    ssize_t bytes;
+    PMPI_Type_size(datatype, &bytes);
+    bytes *= count;
     int comm_f = PMPI_Comm_c2f(comm);
     uint32_t req_f = jenkins_one_at_a_time_hash((char *)request, sizeof(MPI_Request));
 
-    register_irecv(cycles_lo, cycles_hi, bytes, comm_f,
+    register_irecv(cycles_lo, cycles_hi, (size_t)bytes, comm_f,
                    req_f, proc_rank, source, tag);
     return ret;
 }
