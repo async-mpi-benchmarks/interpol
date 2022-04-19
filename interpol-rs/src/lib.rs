dc08288a33e2994ce192b99cac936c9e2eceb93b
dssgabriel <dss.gabriel@protonmail.com>
2022-04-19 02:56:48 +0200

    Added nightly `try_reserve_kind` feature to the crate

--- interpol-rs/src/lib.rs
+++ interpol-rs/src/lib.rs
@@ -1,9 +1,5 @@
+#![feature(try_reserve_kind)]
+
 pub mod interpol;
 pub mod mpi_events;
 pub mod types;
-
-pub(crate) mod mpi_consts {
-    use crate::types;
-
-    pub const MPI_COMM_WORLD: types::MpiComm = 0;
-}
