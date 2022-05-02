e429264a7f9d2fbbe0434d252e9a90f23d47d5e5
dssgabriel <dss.gabriel@protonmail.com>
2022-05-02 17:42:28 +0200

    Added support for `MPI_Ibarrier`

---
+++ interpol-rs/src/mpi_events/synchronization/mpi_ibarrier.rs
@@ -0,0 +1,94 @@
+use crate::interpol::Register;
+use crate::types::{MpiComm, MpiRank, MpiReq, Tsc};
+use derive_builder::Builder;
+use serde::{Deserialize, Serialize};
+use std::collections::TryReserveError;
+
+/// A structure that stores information about `MPI_Barrier` calls.
+///
+/// The following data is gathered when the MPI function is called:
+/// - the rank of the process;
+/// - the communicator on which the barrier was called;
+/// - the request associated with the barrier;
+/// - the current value of the Time Stamp counter before the call to `MPI_Barrier`;
+/// - the duration of the call.
+/// The TSC is measured using the `rdtscp` and `lfence` instructions (see Intel documentation for
+/// further information).
+#[derive(Builder, Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct MpiIbarrier {
+    current_rank: MpiRank,
+    comm: MpiComm,
+    req: MpiReq,
+    tsc: Tsc,
+    duration: Tsc,
+}
+
+impl MpiIbarrier {
+    /// Creates a new `MpiBarrier` structure from the specified parameters.
+    pub fn new(current_rank: MpiRank, comm: MpiComm, req: MpiReq, tsc: Tsc, duration: Tsc) -> Self {
+        Self {
+            current_rank,
+            comm,
+            req,
+            tsc,
+            duration,
+        }
+    }
+}
+
+#[typetag::serde]
+impl Register for MpiIbarrier {
+    fn register(self, events: &mut Vec<Box<dyn Register>>) -> Result<(), TryReserveError> {
+        // Ensure that the program does not panic if allocation fails
+        events.try_reserve_exact(2 * events.len())?;
+        events.push(Box::new(self));
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn builds() {
+        let ibarrier_new = MpiIbarrier::new(0, 0, 0, 1024, 2048);
+        let ibarrier_builder = MpiIbarrierBuilder::default()
+            .current_rank(0)
+            .comm(0)
+            .req(0)
+            .tsc(1024)
+            .duration(2048)
+            .build()
+            .expect("failed to build `MpiIbarrier`");
+
+        assert_eq!(ibarrier_new, ibarrier_builder);
+    }
+
+    #[test]
+    fn serializes() {
+        let ibarrier = MpiIbarrier::new(0, 0, 0, 1024, 2048);
+        let json = String::from("{\"current_rank\":0,\"comm\":0,\"req\":0,\"tsc\":1024,\"duration\":2048}");
+        let serialized = serde_json::to_string(&ibarrier).expect("failed to serialize `MpiIbarrier`");
+
+        assert_eq!(json, serialized);
+    }
+
+    #[test]
+    fn deserializes() {
+        let ibarrier = MpiIbarrierBuilder::default()
+            .current_rank(0)
+            .comm(0)
+            .req(0)
+            .tsc(1024)
+            .duration(2048)
+            .build()
+            .expect("failed to build `MpiIbarrier`");
+        let serialized =
+            serde_json::to_string_pretty(&ibarrier).expect("failed to serialize `MpiIbarrier`");
+        let deserialized: MpiIbarrier =
+            serde_json::from_str(&serialized).expect("failed to deserialize `MpiIbarrier`");
+
+        assert_eq!(ibarrier, deserialized);
+    }
+}
